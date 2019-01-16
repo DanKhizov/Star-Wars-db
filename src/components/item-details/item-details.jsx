e7b6488b222fcaf26071a38d7dcfd62a0740236d
Daniiil Khizov <dan.khizov@gmail.com>
2019-01-16 12:37:03 +0300

    Remove Error Button

--- src/components/item-details/item-details.jsx
+++ src/components/item-details/item-details.jsx
@@ -1,65 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './item-details.css';
-import ErrorButton from '../error-button/error-button';
+import "./item-details.css";
 
 export const Record = ({ item, field, label }) => {
-	return (
-		<li className="list-group-item">
-			<span className="term">{label}</span>
-			<span>{item[field]}</span>
-		</li>
-	);
+  return (
+    <li className="list-group-item">
+      <span className="term">{label}</span>
+      <span>{item[field]}</span>
+    </li>
+  );
 };
 
 export default class PersonDetails extends Component {
-	state = {
-		item: null,
-		image: null,
-	};
+  state = {
+    item: null,
+    image: null
+  };
 
-	componentDidMount() {
-		this.updateItem();
-	}
+  componentDidMount() {
+    this.updateItem();
+  }
 
-	componentDidUpdate(prevProps) {
-		if (this.props.itemId !== prevProps.itemId) {
-			this.updateItem();
-		}
-	}
+  componentDidUpdate(prevProps) {
+    if (this.props.itemId !== prevProps.itemId) {
+      this.updateItem();
+    }
+  }
 
-	updateItem() {
-		const { itemId, getData, getImageUrl } = this.props;
-		if (!itemId) return;
+  updateItem() {
+    const { itemId, getData, getImageUrl } = this.props;
+    if (!itemId) return;
 
-		getData(itemId).then(item => {
-			this.setState({
-				item,
-				image: getImageUrl(item),
-			});
-		});
-	}
+    getData(itemId).then(item => {
+      this.setState({
+        item,
+        image: getImageUrl(item)
+      });
+    });
+  }
 
-	render() {
-		const { item, image } = this.state;
-		if (!item) return <span>Select a person from a list</span>;
+  render() {
+    const { item, image } = this.state;
+    if (!item) return <span>Select a person from a list</span>;
 
-		const { name } = item;
+    const { name } = item;
 
-		return (
-			<div className="person-details card">
-				<img className="person-image" src={image} alt="person" />
+    return (
+      <div className="person-details card">
+        <img className="person-image" src={image} alt="person" />
 
-				<div className="card-body">
-					<h4>{name}</h4>
-					<ul className="list-group list-group-flush">
-						{React.Children.map(this.props.children, child => {
-							return React.cloneElement(child, { item });
-						})}
-					</ul>
-					<ErrorButton />
-				</div>
-			</div>
-		);
-	}
+        <div className="card-body">
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            {React.Children.map(this.props.children, child => {
+              return React.cloneElement(child, { item });
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
